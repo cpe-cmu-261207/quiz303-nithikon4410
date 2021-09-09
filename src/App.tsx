f40114a23337c6a382dd74d6da1988da40436c75
nithikon jansanitsri <nithikon_jansanitsri@cmu.ac.th>
2021-09-09 14:27:19 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ function App() {
 
           {/* image and name */}
           <div className="flex space-x-2 items-center">
-            <img className="w-12 h-12 rounded-full" src="/profileImages/handsome.jpg"></img>
-            <span className='font-semibold text-lg'>Chayanin Suatap 610631100</span>
+            <img className="w-12 h-12 rounded-full" src="/profileImages/nithikon.jpg"></img>
+            <span className='font-semibold text-lg'>Nithikon Jansanitsri 630612102</span>
           </div>
 
           {/* status message */}
@@ -36,7 +36,7 @@ function App() {
             <img className="w-10 w-10 rounded-full" src="/profileImages/lisa.jpg"></img>
             <div className="bg-gray-200 rounded-lg p-2">
               <p className="font-semibold">Lisa</p>
-              <p>ตัวอย่าง Template จ้า</p>
+              <p>จริงค่า</p>
               {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
               <div className='flex items-center'>
                 <img className='w-4 h-4 mr-1' src='/like.svg'></img>
@@ -51,11 +51,40 @@ function App() {
             <img className="w-10 w-10 rounded-full" src="/profileImages/puppy.jpg"></img>
             <div className="bg-gray-200 rounded-lg p-2">
               <p className="font-semibold">หมาน้อย</p>
-              <p>เม้นค้าบ</p>
+              <p>จริงค้าบบบบบบบบ</p>
+            </div>
+          </div>
+
+
+          {/* replies 2 */}
+          <div className="flex p-2 items-start space-x-2 pl-14">
+            <img className="w-10 w-10 rounded-full" src="/profileImages/popcat.png"></img>
+            <div className="bg-gray-200 rounded-lg p-2">
+              <p className="font-semibold">แมวหนึ่งตัว</p>
+              <p>ลิซ่าาาาาาา</p>
+              <div className='flex items-center'>
+                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                <p className='text-gray-500'>2 คน</p>
+              </div>
             </div>
           </div>
 
         </div>
+
+
+        {/* comment */}
+        <div className="flex p-2 items-start space-x-2">
+            <img className="w-10 w-10 rounded-full" src="/profileImages/charliebrown.jpg"></img>
+            <div className="bg-gray-200 rounded-lg p-2">
+              <p className="font-semibold">Charile Brown</p>
+              <p>บ้าไปแล้ว</p>
+              <div className='flex items-center'>
+                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                <p className='text-gray-500'>207 คน</p>
+              </div>
+            </div>
+          </div>
+
 
       </div>
     </div>
